96d03f43fe4fe8450742c2a94276d014e6a8d053
Tarun Padath <tarunpadath@gmail.com>
2020-05-28 17:45:57 -0700

    completed reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
-
+function reverseInt(n) {
+  const sign = Math.sign(n);
+  const reverseStr = n.toString().split("").reverse().join("");
+  return parseInt(reverseStr) * sign;
+}
+// reverseInt(-51);
 module.exports = reverseInt;

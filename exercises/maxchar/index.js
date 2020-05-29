0229048627efde612a2176c8beac920f93c31cda
Tarun Padath <tarunpadath@gmail.com>
2020-05-28 18:23:59 -0700

    finished maxchar question

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charObj = {};
+  let maxC = "";
+  let maxVal = 0;
+  for (let char of str) {
+    if (!charObj[char]) {
+      charObj[char] = 1;
+    } else {
+      charObj[char] += 1;
+    }
+  }
+  for (let objChar in charObj) {
+    if (charObj[objChar] > maxVal) {
+      maxVal = charObj[objChar];
+      maxC = objChar;
+    }
+  }
+  return maxC;
+}
 
 module.exports = maxChar;
